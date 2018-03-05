f82f729d1ce784929d6f66718acdee39888a0e8c
Andy Boutte <andy.boutte@reancloud.com>
2018-03-04 22:15:27 -0600

    adding support for aliases

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
 name 'role-base'
 maintainer 'Andy Boutte'
-maintainer_email 'andy'
+maintainer_email 'andyboutte@gmail.com'
 license 'All Rights Reserved'
 description 'Installs/Configures role-base'
 long_description 'Installs/Configures role-base'
@@ -8,3 +8,4 @@ version '0.1.0'
 chef_version '>= 12.1' if respond_to?(:chef_version)
 
 depends 'yum-epel', '~> 3.0.0'
+depends 'magic_shell', '~> 1.0.0'
