afcfd6c10f718cfff64188ac6889ee8303db2e48
Andy Boutte <andy.boutte@reancloud.com>
2018-06-27 15:31:55 -0400

    minor enhancements

--- test/integration/default/inspec/default_test.rb
+++ test/integration/default/inspec/default_test.rb
@@ -1,17 +1,14 @@
 # # encoding: utf-8
 
-commands = ['dig', 'nc', 'tcpdump', 'tshark', 'strace', 'curl', 'wget', 'vim', 'git', 'jq', 'htop', 'strace', 'unzip', 'zip', 'traceroute']
+commands = ['dig', 'nc', 'tcpdump', 'tshark', 'strace', 'curl', 'wget', 'vim', 'git', 'jq', 'htop', 'strace', 'unzip', 'zip', 'traceroute', 'python', 'aws']
 
-
+commands.each do |cmd|
+  describe bash("command -v #{cmd}") do
+    its('exit_status') { should eq 0 }
+  end
+end
 
 if os[:family] == 'redhat'
-
-  commands.each do |cmd|
-    describe bash("command -v #{cmd}") do
-      its('exit_status') { should eq 0 }
-    end
-  end
-
   describe file('/etc/yum.repos.d/epel.repo') do
     it { should exist }
   end
