47f931f9830df36508f1ed97c784260da8aec732
Andy Boutte <andy.boutte@reancloud.com>
2019-01-29 23:26:03 -0600

    adding amazonlinux2

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,7 +1,7 @@
 
-os_family = node['platform_family']                 # rhel, debian
-os = node['platform']                               # redhat, centos, ubuntu
-os_version = node['platform_version'].split('.')[0] # 7, 6, 14, 16
+os_family = node['platform_family']                 # rhel, debian, amazon
+os = node['platform']                               # redhat, centos, ubuntu, amazon
+os_version = node['platform_version'].split('.')[0] # 7, 6, 14, 16, 2
 
 default['role-base']['packages'] = %w(curl wget vim git jq htop strace iptraf net-tools unzip zip tcpdump wireshark traceroute)
 
@@ -20,6 +20,8 @@ when 'ubuntu'
 end
 
 case os + os_version
+when 'amazon2'
+  node.default['role-base']['packages'].push('nc')
 when 'redhat6'
   node.default['role-base']['packages'].push('nc', 'lynx', 'sysstat')
 when 'redhat7'
